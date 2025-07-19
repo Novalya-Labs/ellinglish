2d21d7493c4dc27d0f3a4adfdf672bb910f1f91e
Enzo Candotti <enzo@creodot.com>
2025-07-20 00:23:43 +0200

    Update version number to 1.0.1 in app.config.ts and package.json

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import type { ExpoConfig } from 'expo/config';
 const config: ExpoConfig = {
   name: 'Ellinglish',
   slug: 'ellinglish',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'ellinglish',
