3700747129fa2d76a8ff47bb42832a0f4c037b69
Enzo Candotti <enzo@creodot.com>
2025-07-20 00:51:34 +0200

    Update metro.config.js to disable unstable package exports and streamline Sentry integration

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,8 @@
 const { withNativeWind } = require('nativewind/metro');
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = withNativeWind(getSentryExpoConfig(__dirname), { input: './global.css' });
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
